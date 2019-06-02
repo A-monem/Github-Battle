d5337ad06df39f61ec51fea7bcdf2e6870450e2f
Abdulmonem Nafea <abdulmonemamin@hotmail.com>
2019-06-02 17:27:22 +1000

    upload all files to github

---
+++ app/utils/api.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID";
+const params = `?client_id=${id}&client_secret=${sec}`;
+
+function getProfile (username) {
+  return axios.get(`https://api.github.com/users/${username}${params}`)
+    .then(({data}) => data);
+}
+
+function getRepos (username) {
+  return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`);
+}
+
+function getStarCount (repos) {
+  return repos.data.reduce((count, {stargazers_count}) => count + stargazers_count, 0);
+}
+
+function calculateScore ({followers}, repos) {
+  const totalStars = getStarCount(repos);
+
+  return (followers * 3) + totalStars;
+}
+
+function handleError (error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData (player) {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(([profile , repos]) => {
+    return {
+      profile,
+      score: calculateScore(profile, repos)
+    }
+  });
+}
+
+function sortPlayers (players) {
+  return players.sort((a,b) =>  b.score - a.score);
+}
+
+module.exports = {
+  battle(players) {
+    return axios.all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError);
+  },
+  fetchPopularRepos(language) {
+    var encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+    return axios.get(encodedURI)
+      .then(response => response.data.items);
+  }
+};
